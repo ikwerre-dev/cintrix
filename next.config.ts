83d21a6af22818cf496474f6f4e55e93f078f653
ikwerre-dev <investorhonour@gmail.com>
2025-10-31 14:36:12 +0100

    added something

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,16 @@ const nextConfig: NextConfig = {
   },
   experimental: {
     optimizeCss: true,
+    esmExternals: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+    }
+    return config;
   },
 }
 
